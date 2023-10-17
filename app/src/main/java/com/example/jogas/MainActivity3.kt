993bffc180b04c6636b238fb28f9fa3ffd75e670
izalfront <rizalhajakubastarus@gmail.com>
2023-10-17 12:14:11 +0800

    penambahan halaman profile joki

--- app/src/main/java/com/example/jogas/MainActivity3.kt
+++ app/src/main/java/com/example/jogas/MainActivity3.kt
@@ -1,12 +1,18 @@
 package com.example.jogas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
-
+        val Tombol = findViewById<ImageButton>(R.id.content_asisten1)
+        Tombol.setOnClickListener {
+            val intent = Intent(this, MainActivity5::class.java)
+            startActivity(intent)
+        }
     }
 }
